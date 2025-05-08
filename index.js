6d688f7b6183ca631b8fe9d1827ca15054bd4248
Lexy Perry <lexy.perry61022@gmail.com>
2025-05-07 22:26:59 -0400

    Finished, tried to fix greater than 100 issue but was unsuccessful

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+function calculateTax(amount) {
+    const tax = amount * 0.10;
+    return tax;
+};
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2) {
+    if(num1 > num2) {
+        return num1; 
+    }
+    else {
+        return num2;
+
+    }
+}
+function isPalindrome(word) {
+    const cleaned = word.toLowerCase();
+    return cleaned === cleaned.split('').reverse().join('');
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    if (discountPercentage < 0) {
+        discountPercentage = 0;
+    }
+    if (discountPercentage > 100) {
+        discountPercentage = 100; 
+    }
+
+    const discount = (discountPercentage / 100) * originalPrice;
+    return originalPrice - discount;
+}
+
 
 
 
